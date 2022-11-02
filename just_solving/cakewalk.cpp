db12f72b8154c65b2a683b46f0ffced5347ddaca
John Nixon <johnbrightnixon@gmail.com>
2022-11-02 12:31:35 +0530

    Added problems solved today

---
+++ just_solving/cakewalk.cpp
@@ -0,0 +1,129 @@
+// Code Written by : John Nixon
+// Date: 02:11:2022  Time: 11:39:22
+// Copyrights are applicable
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define mod 1e9 + 7
+#define F first
+#define S second
+#define pb push_back
+#define si set<int>
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpi vector<pii>
+#define vpp vector<pair<int, pii>>
+#define mii map<int, int>
+#define mpi map<pii, int>
+#define spi set<pii>
+#define endl "\n"
+#define sz(x) ((int)x.size())
+#define all(p) p.begin(), p.end()
+#define double long double
+#define que_max priority_queue<int>
+#define que_min priority_queue<int, vi, greater<int>>
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define print(a)          \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+#define print1(a)    \
+    for (auto x : a) \
+    cout << x.F << " " << x.S << endl
+#define print2(a, x, y)         \
+    for (int i = x; i < y; i++) \
+        cout << a[i] << " ";    \
+    cout << endl
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1)
+            x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+const int N = 200005;
+
+bool cancon(int n, vi a, map<int, bool> &mp)
+{
+    if (mp.find(n) != mp.end())
+    {
+        return mp[n];
+    }
+
+    if (n == 1)
+    {
+        return true;
+    }
+
+    if (n == 0)
+    {
+        return false;
+    }
+
+    for (auto x : a)
+    {
+        int rem = n%x;
+        
+        if (rem == 0 && cancon(n/x, a, mp))
+        {
+            mp[n] = true;
+            return true;
+        }
+    }
+    mp[n] = false;
+    return false;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a = {10, 20};
+
+    map<int, bool> mp;
+
+    bool ans = cancon(n, a, mp);
+
+    // for(auto x:mp){
+    //     cout<<x.F<<" "<<x.S<<endl;
+    // }
+    if (ans)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    clock_t z = clock();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+    return 0;
+}
---
+++ just_solving/saynotodrugs.cpp
@@ -0,0 +1,112 @@
+// Code Written by : John Nixon
+// Date: 01:11:2022  Time: 23:29:21
+// Copyrights are applicable
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define mod 1e9 + 7
+#define F first
+#define S second
+#define pb push_back
+#define si set<int>
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpi vector<pii>
+#define vpp vector<pair<int, pii>>
+#define mii map<int, int>
+#define mpi map<pii, int>
+#define spi set<pii>
+#define endl "\n"
+#define sz(x) ((int)x.size())
+#define all(p) p.begin(), p.end()
+#define double long double
+#define que_max priority_queue<int>
+#define que_min priority_queue<int, vi, greater<int>>
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define print(a)          \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+#define print1(a)    \
+    for (auto x : a) \
+    cout << x.F << " " << x.S << endl
+#define print2(a, x, y)         \
+    for (int i = x; i < y; i++) \
+        cout << a[i] << " ";    \
+    cout << endl
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1)
+            x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+const int N = 200005;
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin >> s;
+
+    if(s.length() == 1 && s[0] == '0'){
+        cout<<1<<endl;
+        return;
+    }
+
+    int count = 0;
+
+    if (s[0] == '0' && s[1] == '0')
+    {
+        s[0] = '1';
+        count++;
+    }
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (s[i - 1] == '0' && s[i] == '0' && s[i + 1] == '0')
+        {
+            s[i] = '1';
+            count++;
+        }
+    }
+
+    if ( s[n - 2] == '0' && s[n - 1] == '0' )
+    {
+        s[n - 1] = '1';
+        count++;
+    }
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    clock_t z = clock();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+    return 0;
+}
